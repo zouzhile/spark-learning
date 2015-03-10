bd2cecc689b7c3508313652b528dba1b7387ea24
Zhile Zou <zouzhile@yahoo-inc.com>
2015-03-10 09:32:28 +0000

    update run script

--- bin/run_wordcount.sh
+++ bin/run_wordcount.sh
@@ -8,19 +8,16 @@
 # see how to configure yarn properties: http://spark.apache.org/docs/latest/running-on-yarn.html 
 # spark configuration guide: http://spark.apache.org/docs/latest/configuration.html#dynamically-loading-spark-properties
 
-export JAVA_HOME=/home/gs/java/jdk64/current
+export JAVA_HOME=/home/gs/java/jdk
 
-export HADOOP_YARN_HOME=$HADOOP_PREFIX
 export HADOOP_PREFIX=/home/gs/hadoop/current
+export HADOOP_YARN_HOME=$HADOOP_PREFIX
 export HADOOP_CONF_DIR=/home/gs/conf/current
 
-export SPARK_HOME=/homes/zouzhile/spark/yspark_root/share/spark/
-export SPARK_JAR=$SPARK_HOME/lib/spark-assembly-1.1.0.1-hadoop0.23.9.jar
-SPARK_CLASSPATH="${SPARK_CLASSPATH}:${HADOOP_PREFIX}/share/hadoop/common/hadoop-gpl-compression.jar:$(ls ${HADOOP_PREFIX}/share/hadoop/hdfs/lib/YahooDNSToSwitchMapping-*.jar):$(ls ${HADOOP_PREFIX}/share/hadoop/common/hadoop-common*.jar | grep -v test | grep -v source):$(ls ${HADOOP_PREFIX}/share/hadoop/common/lib/jetty-util*.jar)"
+export SPARK_BASE=/homes/zouzhile/spark/yspark_root
+export SPARK_HOME=$SPARK_BASE/share/spark/
+export SPARK_JAR=$SPARK_HOME/lib/spark-assembly-1.1.0.1-hadoop2.5.0.1.1408041624.jar
+SPARK_CLASSPATH="${SPARK_CLASSPATH}:/home/gs/hadoop/current/share/hadoop/common/hadoop-gpl-compression.jar:$(ls ${HADOOP_PREFIX}/share/hadoop/hdfs/lib/YahooDNSToSwitchMapping-*.jar)"
 
-export SPARK_YARN_USER_ENV="MALLOC_ARENA_MAX=4"
-export SPARK_YARN_USER_ENV="JAVA_HOME=$JAVA_HOME"
-export SPARK_YARN_USER_ENV="JAVA_HOME=/home/gs/java/jdk64/current,LD_LIBRARY_PATH=/home/gs/hadoop/current/lib/native/Linux-amd64-64/"
-
-$SPARK_HOME/bin/spark-submit --driver-class-path $SPARK_CLASSPATH --class WordCountDriver --master yarn --deploy-mode client --conf "spark.yarn.queue=apg_p7" $PWD/target/spark-examples-1.2.jar
+$SPARK_HOME/bin/spark-submit --driver-class-path $SPARK_CLASSPATH --queue=apg_p7 --class WordCountDriver --master yarn --deploy-mode cluster $PWD/target/spark-examples-1.2.jar
 #$SPARK_HOME/bin/spark-submit --class WordCountDriver --master yarn-client --conf "spark.yarn.queue=apg_p7" $PWD/target/spark-examples-1.2.jar
